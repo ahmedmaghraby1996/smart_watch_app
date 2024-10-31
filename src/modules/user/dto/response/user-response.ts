45407af4a592069c261dd5fc0a58b3ee37df2e82
LENOVO <a.maghraby96@yahoo.com>
2024-10-31 07:31:23 +0300

    create imei

--- src/modules/user/dto/response/user-response.ts
+++ src/modules/user/dto/response/user-response.ts
@@ -1,23 +1,26 @@
-import { Expose, Transform } from "class-transformer";
-import { toUrl } from "src/core/helpers/file.helper";
-import { School } from "src/infrastructure/entities/school/school.entity";
+import { Expose, Transform } from 'class-transformer';
+import { toUrl } from 'src/core/helpers/file.helper';
+import { School } from 'src/infrastructure/entities/school/school.entity';
 
 export class UserResponse {
-    @Expose()
-    id: string;
-    @Expose()
-    name: string;
-    @Expose()
-    email: string;
-    @Expose()
-    @Transform(({ value }) => toUrl(value))
-    avatar: string;
-    @Expose()
-    role: string;
-    @Expose()
-    created_at: Date;
-    @Expose()
-    @Transform(({ value }) => {return value?{id:value.id,name:value.name,avatar:toUrl(value.avatar)}:null})
-    school:School
-
+  @Expose()
+  id: string;
+  @Expose()
+  name: string;
+  @Expose()
+  email: string;
+  @Expose()
+  @Transform(({ value }) => toUrl(value))
+  avatar: string;
+  @Expose()
+  role: string;
+  @Expose()
+  created_at: Date;
+  @Expose()
+  @Transform(({ value }) => {
+    return value
+      ? { id: value.obj.school.id, name: value.obj.school.name, avatar: toUrl(value.obj.school.avatar) }
+      : null;
+  })
+  school: School;
 }
