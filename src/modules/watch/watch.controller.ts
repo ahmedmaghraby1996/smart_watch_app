7770946211a5d19de43a102cd6ed1798e69d31bc
LENOVO <a.maghraby96@yahoo.com>
2024-10-31 07:19:51 +0300

    create imei

--- src/modules/watch/watch.controller.ts
+++ src/modules/watch/watch.controller.ts
@@ -58,7 +58,7 @@ export class WatchController {
   @Post('insert/:IMEI')
   async insert(@Param('IMEI') IMEI: string) {
     return new ActionResponse(
-       this._service.IMEI_repo.create(new IMEI_entity({ IMEI: IMEI })),
+       this._service.IMEI_repo.save(new IMEI_entity({ IMEI: IMEI })),
     );
   }
 
