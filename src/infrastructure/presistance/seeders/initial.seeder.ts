235a526b43a131e8b5b65b758c683aaf12957677
LENOVO <a.maghraby96@yahoo.com>
2024-11-08 12:34:17 +0200

    settings

--- src/infrastructure/presistance/seeders/initial.seeder.ts
+++ src/infrastructure/presistance/seeders/initial.seeder.ts
@@ -1,11 +1,12 @@
 
 
 
+import { StaticPageSeeder } from "./static-pages.seeder";
 import { UsersSeeder } from "./users.seeder";
 
 export const DB_SEEDERS = [
   // UsersSeeder,
   // AddressSeeder,
-
+StaticPageSeeder
 
 ];
