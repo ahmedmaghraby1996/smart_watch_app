41b1a338a2ef45e4353cd4a6f9c45d9cfd5ea25b
LENOVO <a.maghraby96@yahoo.com>
2024-11-09 07:28:47 +0200

    settings

--- src/modules/watch/watch.controller.ts
+++ src/modules/watch/watch.controller.ts
@@ -159,7 +159,7 @@ export class WatchController {
   }
 
   @Roles(Role.PARENT)
-  @Get('/delete-watch-user/:watch_user_id')
+  @Delete('/delete-watch-user/:watch_user_id')
   async deleteWatchUsers(@Param('watch_user_id') watch_user_id: string) {
     return new ActionResponse(await this._service._repo.softDelete({id:watch_user_id,parent_id: this.request.user.id}));
   }
