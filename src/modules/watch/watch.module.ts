0744a238e3a3b957074f1d15506d26e564b5d15f
LENOVO <a.maghraby96@yahoo.com>
2024-11-21 21:31:00 +0200

    add socket

--- src/modules/watch/watch.module.ts
+++ src/modules/watch/watch.module.ts
@@ -4,9 +4,10 @@ import { WatchService } from './watch.service';
 import { FileService } from '../file/file.service';
 import { WatchRequest } from 'src/infrastructure/entities/watch-user/watch-request.entity';
 import { IMEIService, WatchRequestService } from './watch-request.service';
+import { WatchGateway } from 'src/integration/gateways/watch.gateway';
 
 @Module({
   controllers: [WatchController],
-  providers: [WatchService,FileService,WatchRequestService,IMEIService],
+  providers: [WatchService,FileService,WatchRequestService,IMEIService,WatchGateway],
 })
 export class WatchModule {}
