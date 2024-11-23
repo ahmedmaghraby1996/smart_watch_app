f7f949f62f6ba640d6bc65d2993005b22fb13ee5
Ahmed <a.maghraby96@yahoo.com>
2024-11-24 00:35:05 +0200

    error + request

--- src/modules/watch/watch.module.ts
+++ src/modules/watch/watch.module.ts
@@ -5,9 +5,10 @@ import { FileService } from '../file/file.service';
 import { WatchRequest } from 'src/infrastructure/entities/watch-user/watch-request.entity';
 import { IMEIService, WatchRequestService } from './watch-request.service';
 import { WatchGateway } from 'src/integration/gateways/watch.gateway';
+import { NotificationService } from '../notification/services/notification.service';
 
 @Module({
   controllers: [WatchController],
-  providers: [WatchService,FileService,WatchRequestService,IMEIService,WatchGateway],
+  providers: [WatchService,FileService,WatchRequestService,IMEIService,WatchGateway,NotificationService],
 })
 export class WatchModule {}
