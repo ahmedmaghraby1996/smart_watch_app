eae77c7c63adce53e366d16188205b913aca4f22
Ahmed <a.maghraby96@yahoo.com>
2024-11-24 21:08:14 +0200

    edit watch

--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -157,6 +157,7 @@ export class AuthenticationService {
       // Step 6: Extract user information from the decoded token payload
       const userId = decodedToken.payload.sub;
       const email = decodedToken.payload.email as string;
+      console.log(userId, email);
       const access_token = this.jwtService.sign(
         { username: email, sub: userId },
         jwtSignOptions(this._config),
