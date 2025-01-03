365b7ac0147ec1fce6513311de64ab6f52f94415
Ahmed <a.maghraby96@yahoo.com>
2025-01-03 23:27:52 +0200

    send emails

--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -109,7 +109,7 @@ export class AuthenticationService {
           return {
             ...user,role:Role.PARENT,
             access_token: this.jwtService.sign(
-              { username: userInfo.email, sub: userInfo.id },
+              { username: user.email, sub: user.id },
               jwtSignOptions(this._config),
             ),
           };
