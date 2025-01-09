1a50f8c041ff92d46678db67593fbf1b584b5528
Ahmed <a.maghraby96@yahoo.com>
2025-01-09 12:02:30 +0200

    send emails

--- src/infrastructure/entities/school/city.entity.ts
+++ src/infrastructure/entities/school/city.entity.ts
@@ -1,9 +1,10 @@
 import { BaseEntity } from 'src/infrastructure/base/base.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import { School } from './school.entity';
+import { AuditableEntity } from 'src/infrastructure/base/auditable.entity';
 
 @Entity()
-export class City extends BaseEntity {
+export class City extends AuditableEntity {
   @Column()
   name_ar: string;
 
